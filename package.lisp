7f900a9fb567afc1de625b4334ef70124a9dc3d9
Samium Gromoff <_deepfire@feelingofgreen.ru>
2009-04-21 06:21:28 +0400

    [FIX] Export REGINSTANCE-s/BANK/LAYOUT/

--- package.lisp
+++ package.lisp
@@ -18,7 +18,7 @@
    #:layout #:define-layout #:layout-space #:layout-registers #:layout-multi-p #:layout-name-format #:layout-register-selectors
    ;; register stuff
    #:register #:reg-format #:reg-layout #:define-register #:register-decode
-   #:register-instance #:register-instance-by-id #:reginstance-id #:reginstance-device #:reginstance-register #:reginstance-bank #:reginstance-value #:set-reginstance-value
+   #:register-instance #:register-instance-by-id #:reginstance-id #:reginstance-device #:reginstance-layout #:reginstance-register #:reginstance-value #:set-reginstance-value
    #:register-id-valid-for-device-class-p #:register-name-valid-for-device-class-p
    ;; register introspection, SLOW
    #:register-id #:register-by-id #:register-selector
